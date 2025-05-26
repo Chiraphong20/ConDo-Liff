cac5621ed96e35512e9125deece43d9f193abb70
chiraphong <gamemodo6z@gmail.com>
2025-05-26 13:35:39 +0700

    EEEE

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './CSS/index.css';
 import App from './App.jsx'
-
+import VConsole from 'vconsole';
+new VConsole();
+if (import.meta.env.MODE === 'development') {
+  import('vconsole').then((VConsole) => {
+    new VConsole.default();
+  });
+}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
